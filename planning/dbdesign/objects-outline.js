62cd366501eb0da6f182913e0c6d90260abeeae2
Brian Wynn <brian.wynn83@gmail.com>
2015-11-29 14:38:52 -0800

    Setting up some db design and user schemas, along with some planning and asset generation.

---
+++ planning/dbdesign/objects-outline.js
@@ -0,0 +1,22 @@
+// This file lays out principal db design and user object schemas
+
+// USER OBJECT
+  // Name - string
+  // Signup date - string
+  // Last visited - string
+  // Email - string
+  // PW - string
+  // Bikes - array
+  // Previous Searches/ Results - array
+
+// BIKE DB DESIGN
+  // Brand
+    // Model
+      // Year
+      // Name
+      // Type of bike [ mtn, road, hybrid, cruiser ]
+      // Experience [ xc, dh, freeride, all-mtn, recreation ]
+      // Wheel Size
+      // Full Suspension - boolean
+      // Investment
+      // Tags [ endurance, touring ]
