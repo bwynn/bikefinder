fb674eb1170ae4241e91fb45e8f2af839570729f
Brian Wynn <brian.wynn83@gmail.com>
2015-11-30 22:54:19 -0800

    Added image.

---
+++ dbdesign/objects-outline.js
@@ -0,0 +1,29 @@
+// This file lays out principal db design and user object schemas
+
+// USER OBJECT
+  // Name - string
+  // Signup date - string
+  // Last visited - string
+  // Email - string
+  // PW - string
+  // Bikes - array
+  // Previous Searches/ Results - array
+
+// BIKE DB DESIGN
+  // Brand
+    // Model
+      // Year
+      // Name
+      // Type of bike [ mtn, road, hybrid, cruiser ]
+      // Experience [ xc, dh, freeride, all-mtn, recreation ]
+      // Wheel Size
+      // Full Suspension - boolean
+      // Investment
+      // Tags [ endurance, touring ]
+
+
+// UX determines bike suggestions based on
+  // Type of Bike
+  // Experience
+  // Investment
+  // Tags
